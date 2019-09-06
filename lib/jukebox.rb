3aedfb0bb79f0ae6710ec8777318d94260b5d6d1
Joelle Helm <joellenhelm@gmail.com>
2019-09-06 16:16:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -5,3 +5,9 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
+def list(songs)
+  songs.each_with_index do |song, idx|
+    puts "#{idx}. #{song}"
+  end
+end
