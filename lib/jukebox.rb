ced441007554c76b034d78cf9a12df2eddf435f4
Joelle Helm <joellenhelm@gmail.com>
2019-09-06 16:18:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,3 +11,13 @@ def list(songs)
     puts "#{idx}. #{song}"
   end
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  if songs.includes?(input)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
